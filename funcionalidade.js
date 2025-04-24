5948a2058767c1bba02bfd5e3d3d73e1c0b7be01
whesleydiasmaciel <whesleydiasmaciel@gmail.com>
2025-04-24 17:22:56 +0000

    Atividade

--- funcionalidade.js
+++ funcionalidade.js
@@ -5,10 +5,10 @@ function lerIdade() {
 }
 
 function votacao(idade) {
-    document.getElementById("resposta").innerHTML = `<div class="tile">Idade informada: ${idade}</div>`
+    document.getElementById("resposta").innerHTML = `<div class="tile">Idade informada: ${idade}</div>`;
     if (idade < 18)
-        document.getElementById("resposta").innerHTML += `<div class="tile">A pessoa não pode votar!</div>`
+        document.getElementById("resposta").innerHTML += `<div class="tile">A pessoa não pode votar!</div>`;
     else
-        document.getElementById("resposta").innerHTML += `<div class="tile">A pessoa pode votar!</div>`
+        document.getElementById("resposta").innerHTML += `<div class="tile">A pessoa pode votar!</div>`;
 
 }
