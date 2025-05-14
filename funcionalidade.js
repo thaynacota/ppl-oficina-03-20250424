a97ff9d46637ded13ea2671be5f0a4e37ef5f5cf
Thayná Cota <thaynacota390@gmail.com>
2025-05-14 22:11:23 +0000

    Modificação e pernonalização do css

--- funcionalidade.js
+++ funcionalidade.js
@@ -8,11 +8,14 @@ function votacao(idade) {
     document.getElementById("resposta").innerHTML = "<div class='tile' id='mensagem'>"
 
     if (idade < 18) {
-        document.getElementById("mensagem").innerHTML += `<div class="avatar menor-idade">${idade}</div>`;
-        document.getElementById("mensagem").innerHTML += `<div class="corpo">A pessoa não pode votar!</div>`;
-    } else {
-        document.getElementById("mensagem").innerHTML += `<div class="avatar maior-idade">${idade}</div>`;
-        document.getElementById("mensagem").innerHTML += `<div class="corpo">A pessoa pode votar!</div>`;
-    } document.getElementById("resposta").innerHTML += "</div>"
+        document.getElementById("mensagem").innerHTML += `<div class="avatar menor-idade">${idade}</div>`
+        document.getElementById("mensagem").innerHTML += `<div class="corpo">A pessoa <strong>não</strong> pode votar!</div>`
+    }
+
+    else {
+        document.getElementById("mensagem").innerHTML += `<div class="avatar maior-idade">${idade}</div>`
+         document.getElementById("mensagem").innerHTML += `<div class="corpo">A pessoa pode votar!</div>`
+         document.getElementById("resposta").innerHTML += "</div>"
+    }
 
 }
